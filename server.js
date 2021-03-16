9b3f4e5006b2be20cd5d7f3610c261238c7bc247
randy554 <randy554@hotmail.com>
2021-03-16 15:51:34 +0100

    added async to fetch call

--- server.js
+++ server.js
@@ -12,28 +12,71 @@ app.set("views", "views");
 
 app.use(express.static("public"));
 
-// API endpoint
-const api_endpoint = `https://api.giphy.com/v1/gifs/trending?api_key=${api_key}&limit=12&rating=g`;
+// // API endpoint
+// const api_endpoint = `https://api.giphy.com/v1/gifs/trending?api_key=${api_key}&limit=12&rating=g`;
 
-app.get("/", (req, res) => {
-  fetch(api_endpoint)
-    .then((result) => result.json())
-    .then((jsonReturnData) => {
-      res.render("index", {
-        data: jsonReturnData.data,
-      });
+// app.get("/", (req, res) => {
+//   fetch(api_endpoint)
+//     .then((result) => result.json())
+//     .then((jsonReturnData) => {
+//       res.render("index", {
+//         data: jsonReturnData.data,
+//       });
+//     });
+// });
+
+async function getData(apiEndpoint) {
+  const response = await fetch(apiEndpoint);
+  console.log("Response: ", response);
+
+  const data = await response.json();
+  console.log("Data:", data);
+
+  return data;
+}
+
+app.get("/", async (req, res) => {
+  try {
+    const returnData = await getData(
+      `https://api.giphy.com/v1/gifs/trending?api_key=${api_key}&limit=12&rating=g`
+    );
+
+    console.log("Dit is returnData ", returnData);
+
+    res.render("index", {
+      data: returnData.data,
     });
+  } catch (error) {
+    res.send("Error on path: / ");
+  }
 });
 
-app.get("/detail/:id/:username?", (req, res) => {
-  fetch(`https://api.giphy.com/v1/gifs/${req.params.id}?api_key=${api_key}`)
-    .then((result) => result.json())
-    .then((jsonReturnData) => {
-      res.render("detail", {
-        data: jsonReturnData.data,
-        username: req.params.username,
-      });
+// app.get("/detail/:id/:username?", (req, res) => {
+//   fetch(`https://api.giphy.com/v1/gifs/${req.params.id}?api_key=${api_key}`)
+//     .then((result) => result.json())
+//     .then((jsonReturnData) => {
+//       res.render("detail", {
+//         data: jsonReturnData.data,
+//         username: req.params.username,
+//       });
+//     });
+// });
+
+app.get("/detail/:id/:username?", async (req, res) => {
+  try {
+    const returnData = await getData(
+      `https://api.giphy.com/v1/gifs/${req.params.id}?api_key=${api_key}`
+    );
+
+    console.log("Dit is returnData ", returnData);
+
+    res.render("detail", {
+      data: returnData.data,
+      username: req.params.username,
     });
+  } catch (error) {
+    res.send("Error on path: /detail ");
+  }
 });
 
 app.listen(port || 3001, () =>
