6975358a2a9af9f90446f727caea72b60108944d
randy554 <randy554@hotmail.com>
2021-03-16 16:12:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
-const api_key = process.env.API_KEY;
+// const api_key = process.env.API_KEY;
+const api_key = "tuMYha0ArdAhO9rqn9aM9erHww61XGns";
 
 app.set("view engine", "ejs");
 app.set("views", "views");
