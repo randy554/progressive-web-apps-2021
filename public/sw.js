b231b411a8d235f0fa3b2f922c83676ac95436ae
randy554 <randy554@hotmail.com>
2021-03-22 13:24:00 +0100

    added assets to cache

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,30 @@
-// Install service worker
+const staticCacheName = "site-static";
+const assets = [
+  "/",
+  "/css/index.css",
+  "/js/index.js",
+  "https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap",
+  "/images/icons/favicon.png",
+  "/images/icons/Icon-192.png",
+];
+// Listen to the installation of the service worker
 self.addEventListener("install", (evt) => {
   console.log("service worker has been installed");
+
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log("Caching assets");
+      cache.addAll(assets);
+    })
+  );
 });
 
 // Listen to activate event
 self.addEventListener("activate", (evt) => {
   console.log("service worker has been activated");
+});
+
+// Listen to fetch events
+self.addEventListener("fetch", (evt) => {
+  console.log("service worker fetch event has occurred", evt);
 });
